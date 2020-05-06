5868d0454338fa237fb07d1e354cc3bff105c19c
gamjagoon <rlaalswo201@gmail.com>
2020-05-06 12:42:13 +0900

    ee

--- src/contents/Curdata.js
+++ src/contents/Curdata.js
@@ -6,8 +6,8 @@ const Curdata = () => {
 	return (
     <div>
       <Typography variant="h2">
-        PM2.5 : {data[1].pm25}㎍/m³ <br />
-        PM10 : {data[1].pm10}㎍/m³
+        PM2.5 : {data[0].pm25}㎍/m³ <br />
+        PM10 : {data[0].pm10}㎍/m³
         <br />
         {data[1].time}
       </Typography>
