9ded83f933ebcd14ff57e8980924817d3fd556af
gamjagoon <rlaalswo201@gmail.com>
2020-05-10 01:28:21 +0900

    update sensor code

--- src/contents/Curdata.js
+++ src/contents/Curdata.js
@@ -1,15 +1,23 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
-import data from "./pm_data/data.json";
+import obj from "./pm_data/aqi.json";
+//import fs from "fs";
+//let obj = [];
 
 const Curdata = () => {
+//	fs.readFile('/var/www/html/aqi.json',(err, data)=>{
+//		if(err){console.log(err);}
+//		else{
+//			obj= JSON.parse(data);
+//		}
+//	});
 	return (
     <div>
       <Typography variant="h2">
-        PM2.5 : {data[0].pm25}㎍/m³ <br />
-        PM10 : {data[0].pm10}㎍/m³
+        PM2.5 : {obj[0].pm25}㎍/m³ <br />
+        PM10 : {obj[0].pm10}㎍/m³
         <br />
-        {data[1].time}
+        {obj[0].time}
       </Typography>
     </div>
   );
