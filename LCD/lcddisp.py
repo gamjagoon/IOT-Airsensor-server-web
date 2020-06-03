33696fa4a56d7414a07369c9413b4f8db6b05971
gamjagoon <rlaalswo201@gmail.com>
2020-06-03 15:31:44 +0900

    add func display 16x2 char LCD

--- LCD/lcddisp.py
+++ LCD/lcddisp.py
@@ -3,20 +3,34 @@ from time import *
 mylcd = I2C_LCD_driver.lcd()
 pm10 = "pm10 "
 pm25 = "pm25 "
+hum = "hum "
+tem = "tem "
+prefix2 = "'C"
 prefix = "ug/m3"
 while 1 :
-    f = open('./tmp.txt',mode='r')
-    value = f.readline().split()
-    value.pop()
-    value.pop()
-    value = map(float,value)
-    str1 = ""
-    str2 = ""
-    val1 = round(value[0],2)
-    val2 = round(value[1],2)
-    str1 = str1 + pm10 + str(round(val1,2)) + prefix
-    str2 = str2 + pm25 + str(round(val2,2)) + prefix
-    mylcd.lcd_display_string(str1,1)
-    mylcd.lcd_display_string(str2,2)
-    f.close()
-    sleep(30)
+    f1 = open('./tmp.txt',mode='r')
+    value_pm = f1.readline().split()
+    f1.close()
+    f2 = open('./tmp2.txt',mode='r')
+    value_hum = f2.readline().split()
+    f2.close()
+    value_pm.pop()
+    value_pm.pop()
+    value_hum.pop()
+    value_hum.pop()
+    value_pm = list(map(float,value_pm))
+    value_hum = list(map(float,value_hum))
+    strs = list(range(0,4))
+    strs[0] = pm10 + str(round(value_pm[0],2)) + prefix
+    strs[1] = pm25 + str(round(value_pm[1],2)) + prefix
+    strs[2] = hum + str(round(value_hum[0],2)) + "%"
+    strs[3] = tem + str(round(value_hum[1],2)) + prefix2
+    for _ in range(29):
+        mylcd.lcd_display_string(strs[0],1)
+        mylcd.lcd_display_string(strs[1],2)
+        sleep(1)
+        mylcd.lcd_clear()
+        mylcd.lcd_display_string(strs[2],1)
+        mylcd.lcd_display_string(strs[3],2)
+        sleep(1)
+        mylcd.lcd_clear()
