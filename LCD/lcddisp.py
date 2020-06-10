d9f9fcaeb4bc2992ba99a25708d13e4c0927a343
gamjagoon <rlaalswo201@gmail.com>
2020-06-10 13:55:58 +0900

    commit

--- LCD/lcddisp.py
+++ LCD/lcddisp.py
@@ -6,17 +6,28 @@ pm25 = "pm25 "
 prefix = "ug/m3"
 while 1 :
     f = open('./tmp.txt',mode='r')
-    value = f.readline().split()
-    value.pop()
-    value.pop()
-    value = map(float,value)
-    str1 = ""
-    str2 = ""
-    val1 = round(value[0],2)
-    val2 = round(value[1],2)
-    str1 = str1 + pm10 + str(round(val1,2)) + prefix
-    str2 = str2 + pm25 + str(round(val2,2)) + prefix
-    mylcd.lcd_display_string(str1,1)
-    mylcd.lcd_display_string(str2,2)
+    value = list(f.readline().split())
+    value = value[0:2]
+    print(value)
     f.close()
-    sleep(30)
+    f2 = open('./tmp2.txt',mode='r')
+    value2 = list(f2.readline().split())
+    value2 = value2[0:2]
+    f2.close()
+    val1 = round(float(value[0]),2)
+    val2 = round(float(value[1]),2)
+    str1 = pm10 + str(val1) + prefix
+    str2 = pm25 + str(val2) + prefix
+    str3 = "hum:" + value2[0] + "%"
+    str4 = "tem:"+ value2[1] + "C"
+    for i in range(15):
+        mylcd.lcd_display_string(str1,1)
+        mylcd.lcd_display_string(str2,2)
+        sleep(2)
+        mylcd.lcd_clear()
+        mylcd.lcd_display_string(str3,1)
+        mylcd.lcd_display_string(str4,2)
+        sleep(2)
+        mylcd.lcd_clear()
+
+
