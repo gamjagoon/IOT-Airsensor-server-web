49e14d12bf9d7f15682784ed15e80c71fe704e6a
gamjagoon <rlaalswo201@gmail.com>
2020-06-16 23:47:23 +0900

    client

--- tcpclient.py
+++ tcpclient.py
@@ -1,1 +1,13 @@
-import
+import socket,sys,time
+
+def run():
+    with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as S:
+        S.connect(('algora.iptime.org',30122))
+        data = "1.22 1.23 52.6 29.87 2020.06.16 23:44"
+        time.sleep(1)
+        S.sendall(data.encode())
+        resp = S.recv(64)
+        print(resp)
+
+if __name__ == "__main__":
+    run()
